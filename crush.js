2a2694d6729ea3a98428dd832feeae8609d4dca9
codexsheesh <127030222+codexsheesh@users.noreply.github.com>
2023-03-05 21:31:35 +0800

    Add files via upload

---
+++ crush.js
@@ -0,0 +1,14 @@
+const button1=document.querySelector(".btn1");
+const button2=document.querySelector(".btn2");
+var text1=document.getElementById("text");
+button1.addEventListener("click",function(event){
+text1.textContent="No choice pre";
+event.preventDefault();
+});
+
+button2.addEventListener("mouseover",function(event){
+const i=Math.floor(Math.random()*600)+1;
+const j=Math.floor(Math.random()*500)+1;
+button2.style.left=i+"px";
+button2.style.top=j+"px";
+});
