e011657624c7d0e3ace2eef760ebc6aa0e998289
psuuc-jbgarcia <127030222+psuuc-jbgarcia@users.noreply.github.com>
2024-08-02 21:01:07 +0800

    Update crush.js

--- crush.js
+++ crush.js
@@ -1,14 +1,16 @@
-const button1=document.querySelector(".btn1");
-const button2=document.querySelector(".btn2");
-var text1=document.getElementById("text");
-button1.addEventListener("click",function(event){
-text1.textContent="Yehey I love you";
-event.preventDefault();
+const button1 = document.querySelector(".btn1");
+const button2 = document.querySelector(".btn2");
+var text1 = document.getElementById("text");
+
+button1.addEventListener("click", function(event) {
+    text1.textContent = "Yehey I love you";
+    event.preventDefault();
 });
 
-button2.addEventListener("mouseover",function(event){
-const i=Math.floor(Math.random()*600)+1;
-const j=Math.floor(Math.random()*500)+1;
-button2.style.left=i+"px";
-button2.style.top=j+"px";
+button2.addEventListener("mouseover", function(event) {
+    const i = Math.floor(Math.random() * 600) + 1;
+    const j = Math.floor(Math.random() * 500) + 1;
+    button2.style.position = 'absolute';
+    button2.style.left = i + "px";
+    button2.style.top = j + "px";
 });
